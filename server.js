6a7ed15c8d60e2e58f3d19e5257c4e3f675e59c4
Steve Walker <captnwalker@gmail.com>
2018-01-18 17:27:47 -0500

    server.js connecting true on 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 
 var path = require('path');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Setting up Express Server
 var express = require('express')
@@ -16,7 +16,6 @@ app.get('/', function (req, res) {
 })
  
 
-
 // BodyParser to sort JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,12 +23,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // Requires for local .js files
-// require('./app/routing/api-routes.js')(app); 
-// require('./app/routing/html-routes.js')(app);
-
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
 // Start the server
-
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
