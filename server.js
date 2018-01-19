d4030d8964373a40031c87516b5dacaa2294c4a7
Steve Walker <captnwalker@gmail.com>
2018-01-18 20:23:47 -0500

    troubleshooting  server.js

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-// dependencies
-var http = require("http");
-
-var bodyParser = require('body-parser')
-
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 
-// setting up Express Server
-var express = require('express');
+// Configure the Express application
 var app = express();
- 
-app.get('/', function (req, res) {
-});
-//   res.send('Hello World!!!!!')
+var PORT = process.env.PORT;
 
-var PORT = process.env.PORT || 8080;
-
-// bodyParser to sort JSON
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
+// Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
 
-// requires for local .js files
-require('./app/routing/apiRoutes.js')(app); 
-require('./app/routing/htmlRoutes.js')(app);
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
-// start the server
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
 app.listen(PORT, function() {
-	console.log("Zombie Friend Finder App is listening on PORT: " + PORT);
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
